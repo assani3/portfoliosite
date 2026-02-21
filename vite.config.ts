17ecc3861cda38eca4dfc62135479a31e05a1db3
assani3 <assanishabani51@gmail.com>
2026-02-21 19:05:41 +0200

    gh-pages assani

--- vite.config.ts
+++ vite.config.ts
@@ -45,6 +45,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: '/portfoliosite/',
   plugins: [react()],
   resolve: {
     alias: {
